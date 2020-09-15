e269b6546004d73fdaa8c983b9f1320703e0982e
someday <somedaymail@163.com>
2020-09-15 20:43:43 +0800

    spring cloud 版本升级

--- tuning-center/test-kafka-spring-boot-starter/producer/src/test/java/com/open/capatcity/message/service/ProducerServiceTest.java
+++ tuning-center/test-kafka-spring-boot-starter/producer/src/test/java/com/open/capatcity/message/service/ProducerServiceTest.java
@@ -1,7 +1,8 @@
-package com.open.capatcity.message.service; 
+package com.open.capatcity.message.service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 import javax.annotation.Resource;
 
@@ -14,24 +15,24 @@ import com.open.capatcity.message.MessageApp;
 
 import lombok.extern.slf4j.Slf4j;
 
- 
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {
-		MessageApp.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // 配置启动类
+@SpringBootTest(classes = { MessageApp.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // 配置启动类
 public class ProducerServiceTest {
-	
+
 	@Resource
-	private ProducerService producerService ;
-	
+	private ProducerService producerService;
+
 	@Test
 	public void test() {
-		
-		Map map = new HashMap();
-		map.put("msg", "hello");
-		boolean flag = producerService.sendMsg(map);
-		System.out.println(flag);
+
+		IntStream.rangeClosed(1, 50000).forEach((i) -> {
+			Map map = new HashMap<>();
+			map.put("hello", "world") ;
+			boolean flag = producerService.sendMsg(map);
+			System.out.println(flag);
+		});
+
 	}
 
-	
 }
