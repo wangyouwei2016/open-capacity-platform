e2757af1778bbd8362d862476169eba8c96b1c2b
hillChen <chenxiaoshan01@zhengbang.com>
2023-03-20 23:08:41 +0800

    添加hilltool扩展组件

--- inner-intergration/base-spring-boot-starter/src/main/java/com/open/capacity/ext/core/ResetDefinitionAutoConfig.java
+++ inner-intergration/base-spring-boot-starter/src/main/java/com/open/capacity/ext/core/ResetDefinitionAutoConfig.java
@@ -1,3 +1,6 @@
+/**
+* 相关源码实现原理和使用手册请查看:https://gitee.com/hilltool/hill-spring-ext
+*/
 package com.open.capacity.ext.core;
 
 import org.springframework.context.annotation.Bean;
--- inner-intergration/base-spring-boot-starter/src/main/java/com/open/capacity/ext/iop/feign/FeignIopClientsDefiner.java
+++ inner-intergration/base-spring-boot-starter/src/main/java/com/open/capacity/ext/iop/feign/FeignIopClientsDefiner.java
@@ -1,4 +1,7 @@
 
+/**
+* 相关源码实现原理和使用手册请查看:https://gitee.com/hilltool/hill-spring-ext
+*/
 package com.open.capacity.ext.iop.feign;
 
 import com.open.capacity.ext.iop.IopClientsDefiner;
